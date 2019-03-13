f70b6b131411203b1bbc4e5d4740b0f4fa990a5b
崔家郡 <jiajunc.18@intl.zju.edu.cn>
2019-03-13 14:59:12 +0800

    variable and collatz

---
+++ Practical4/variable.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 13 09:17:51 2019
+
+@author: cuijiajun
+"""
+
+a = 123
+b =123123
+if b % 7==0 :
+    print ('true')
+
+c = b/7
+d =c/11
+e = d/13
+if e==a:
+    print ('same!')
