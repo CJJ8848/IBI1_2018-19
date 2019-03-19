5cc328599b1936b78fa42d0d5e3f27cab531fb80
崔家郡 <jiajunc.18@intl.zju.edu.cn>
2019-03-19 17:55:09 +0800

    task 1

---
+++ practical 5/counting DNA Nucleotides.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 19 17:40:16 2019
+
+@author: cuijiajun
+"""
+
+DNA = input ('give me a sequence of DNA: ')
+mydict = {}
+for word in DNA :
+    if word in mydict :
+        mydict[word] += 1
+    else :
+        mydict[word] = 1
+print (mydict)
+
+import matplotlib.pyplot as plt
+
+labels = 'A','T','C','G'
+sizes = [int(mydict['A']),int(mydict['T']), int(mydict['C']), int(mydict['G']) ]
+explode = (0,0,0,0)
+plt.pie(sizes, explode=explode, labels =labels, autopct = '%1.1f%%', shadow = True, startangle= 90)
+plt.axis('equal')
+
+plt.show()
