e4044f1b059c5db8d445069de5cb06b24a312585
崔家郡 <jiajunc.18@intl.zju.edu.cn>
2019-03-21 10:22:35 +0800

    task done

--- practical 5/counting DNA Nucleotides.py
+++ practical 5/counting DNA Nucleotides.py
@@ -5,22 +5,31 @@ Created on Tue Mar 19 17:40:16 2019
 
 @author: cuijiajun
 """
-
+# input some sequence
 DNA = input ('give me a sequence of DNA: ')
+# construct mydict
 mydict = {}
+# show the times words appear
 for word in DNA :
     if word in mydict :
         mydict[word] += 1
     else :
         mydict[word] = 1
+# output
 print (mydict)
-
+# get a plot
 import matplotlib.pyplot as plt
-
+# give the labels
 labels = 'A','T','C','G'
+# give the sizes of labels
 sizes = [mydict['A'],mydict['T'], mydict['C'], mydict['G'] ]
+# choose which labels to go out of a pie
 explode = (0,0,0,0)
+# autopct : means the number will in the shape of 1.0, 
+# shadow: choose whether pie has shadow 
+# startangle : it is beautiful if the startgngle is 90
+# plt.axis ('equal'): make the pie be a circle.  
 plt.pie(sizes, explode=explode, labels =labels, autopct = '%1.1f%%', shadow = True, startangle= 90)
 plt.axis('equal')
-
+# output
 plt.show()
--- practical 5/string reverse and list sorting .py
+++ practical 5/string reverse and list sorting .py
@@ -5,10 +5,15 @@ Created on Wed Mar 20 09:01:14 2019
 
 @author: cuijiajun
 """
-
+# input some words
 string = input ('give me a string of words: ')
+# reverse the elements
 string =  string [::-1]
+# split the string with space
 string = string.split ()
+# order the string 
 string.sort()
+# reverse the order
 string.reverse()
+# output
 print (string)
