6fd979efe741d523351e1128a85dd1f2bd299ba1
崔家郡 <jiajunc.18@intl.zju.edu.cn>
2019-04-03 11:37:54 +0800

    practical 7

--- practical 7/24 points.py
+++ practical 7/24 points.py
@@ -13,16 +13,16 @@ for i in n:
      m.append(int(i))
      if int(i) >=24:
          print('The input number must be integers from 1 to 23')
-        
-r=0   
+         break
 if int(i)<=24:
-
+    r=0
     def compute(x,y,op):
         if op=='+':return x+y
         elif op=='*':return x*y
         elif op=='-':return x-y
         else:return x/y if y else None
     def exp(p,iter=0):
+    
         from itertools import permutations
         if len(p)==1:return [(p[0],str(p[0]))]
         operation = ['+','-','*','/']
@@ -33,12 +33,16 @@ if int(i)<=24:
             r=r+1
             #print(array_n)
             for num in range(1,len(array_n)):
+                r=r+1
                 ret1 = exp(array_n[:num],iter+1)
                 ret2 = exp(array_n[num:],iter+1)
-                for op in operation:
+                for op in operation: 
+                    r=r+1
                     for va1,expression in ret1:
+                        r=r+1
                         if va1==None:continue
                         for va2,expression2 in ret2:
+                            r=r+1
                             if va2==None:continue
                             combined_exp = '{}{}' if expression.isalnum() else '({}){}'
                             combined_exp += '{}' if expression2.isalnum() else '({})'
@@ -46,7 +50,16 @@ if int(i)<=24:
                             ret.append((new_val,combined_exp.format(expression,op,expression2)))
                             if iter==0 and new_val==24:
         
-                                return 'yes'#''.join(e+'/n' for x,e in ret if x==24),
+                                return 'yes',''.join(e+'' for x,e in ret if x==24)
         return ret
-    print(exp(m))
-    print('recursion times:',r)
+    import re
+    if re.search(r'yes',str(exp(m))):
+        print(exp(m))
+        print('recursion times:',r)
+    else:
+        print('no output')
+
+    
+       
+       
+        
