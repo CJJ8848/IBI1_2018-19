0d6e48e810f5983d77279b5babb5b0515041dcb0
崔家郡 <jiajunc.18@intl.zju.edu.cn>
2019-04-11 11:06:05 +0800

    improved after tutorial practical 8

--- practical 8/find go term.py
+++ practical 8/find go term.py
@@ -5,38 +5,17 @@ Created on Tue Apr  9 17:38:23 2019
 
 @author: cuijiajun
 """
-import re
-#from xml.dom.minidom import parse
-import xml.dom.minidom
-DOMTree = xml.dom.minidom.parse ("go_obo.xml")
-collection = DOMTree.documentElement
-terms = collection.getElementsByTagName ("term")
-geneslist = [["id", "name", "defination","count"],]
-genelist = []
-parentlist = []
-def count_childnode (GOID):
-    childnode = 0
+def Child (id , resultSet):
     for term in terms:
-        is_a = term.getElementsByTagName ("is_a")
-        parentlist.append(is_a)
-    if re.search (GOID,str(parentlist)):
-        childnode += 1
-    return childnode
-  
-for term in terms:
-    id = term.getElementsByTagName ("id")[0].childNodes[0].nodeValue
-    name = term.getElementsByTagName ("name")[0].childNodes[0].nodeValue
-    #is_a = term.getElementsByTagName ("is_a")[0].childNodes[0].nodeValue
-    defstr = term.getElementsByTagName ("defstr")[0].childNodes[0].nodeValue
-       
-    if re.search('autophagosome',defstr):
-        countchild = count_childnode(id)
-        genelist = [id,name,defstr,countchild] 
-        geneslist.append (genelist)
-
-import openpyxl
-
-
+        # parents is a list containing all parents of one term
+        parents = term.getElementsByTagName('is_a')
+        # geneid is a string, the id of one term 
+        geneid = term.getElementsByTagName('id')[0].childNodes[0].data
+        # for each parent of one term if there is a id in parents, add the geneid of one term(a child)
+        for parent in parents:
+            if parent.childNodes[0].data == id:
+                resultSet.add(geneid)
+                Child(geneid, resultSet)
 def write_excel_xlsx(path, sheet_name, value):
     index = len(value)
     workbook = openpyxl.Workbook()
@@ -47,10 +26,27 @@ def write_excel_xlsx(path, sheet_name, value):
             sheet.cell(row=i+1, column=j+1, value=str(value[i][j]))
     workbook.save(path)
     print("success!")
+import re
+import xml.dom.minidom
+DOMTree = xml.dom.minidom.parse ("go_obo.xml")
+collection = DOMTree.documentElement
+terms = collection.getElementsByTagName ("term")
+geneslist = [["id", "name", "defination","count"],]
+genelist = []
+for term in terms:
+    id = term.getElementsByTagName ("id")[0].childNodes[0].nodeValue
+    name = term.getElementsByTagName ("name")[0].childNodes[0].nodeValue
+    defstr = term.getElementsByTagName ("defstr")[0].childNodes[0].nodeValue
+       
+    if re.search('autophagosome',defstr):
+        resultSet = set()
+        Child(id,resultSet)
+        countchild = len(resultSet)
+        genelist = [id,name,defstr,countchild] 
+        geneslist.append (genelist)
+
+import openpyxl
 book_name_xlsx = 'autophagosome (myself).xlsx'
- 
-sheet_name_xlsx = 'autophagosome genes'
- 
+sheet_name_xlsx = 'autophagosome genes' 
 value =  geneslist#elementCount]]
- 
 write_excel_xlsx(book_name_xlsx, sheet_name_xlsx, value)
